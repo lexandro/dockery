54958d51b3856574db5cffc6ef0a56579eb6c5e2
lexandro <lexandro2000@gmail.com>
2015-03-23 22:53:51 +0100

    Adding image details

--- app/app.modules.js
+++ app/app.modules.js
@@ -9,10 +9,12 @@ angular.module('dockermon', [
     'services',
     'containers',
     'containerDetails',
-    'images'
+    'images',
+    'imageDetais'
 ])
     .run(function ($rootScope) {
         $rootScope.host = "http://devft-docker-host-02.web.zooplus.de:2375";
+        $rootScope.host = "http://192.168.100.29:2375";
         //
         //$rootScope.sidemenu = 'app/shared/sidemenu.html';
         console.log("Rootscope initialized");
