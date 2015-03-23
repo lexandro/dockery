8c4d49cbab7185908c0b8b8afb257bbe1477b7e5
lexandro <lexandro2000@gmail.com>
2015-03-23 22:54:12 +0100

    Adding button to show container details

--- app/containers/containers.js
+++ app/containers/containers.js
@@ -8,12 +8,34 @@ angular.module('containers', ['ngRoute'])
         });
     }])
     .controller('ContainerCtrl', ['$rootScope', '$scope', '$location', 'Containers', function ($rootScope, $scope, $location, Containers) {
-        console.log('1')
+
         var containers = Containers.query(function () {
-            $scope.containers = containers;
+            var containerDataList = [];
+            containers.forEach(function (container) {
+                var containerData = {};
+                containerData.Id = container.Id;
+                containerData.Image = container.Image;
+                containerData.Command = container.Command;
+                containerData.Created = container.Created;
+                containerData.Status = container.Status;
+                containerData.Ports = container.Ports;
+                containerDataList.push(containerData);
+
+                var containerDetails = Containers.get({containerId: container.Id}, function () {
+                    $scope.containerDetails = containerDetails;
+                    containerData.ImageId = containerDetails.Image;
+                });
+
+            });
+            $scope.containerDataList = containerDataList;
         });
-        $scope.go = function (path) {
-            $location.path(path);
+        //
+        $scope.goContainerDetails = function (path) {
+            $location.path('/containerDetails/' + path);
+        };
+        //
+        $scope.goImageDetails = function (path) {
+            $location.path('/imageDetails/' + path);
         };
     }])
 ;
