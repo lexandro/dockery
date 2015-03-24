8c16751e2ce138c6a7198667e5661c2a593c6e15
lexandro <lexandro2000@gmail.com>
2015-03-24 23:13:26 +0100

    Removing hardcoded host urls

--- app/app.modules.js
+++ app/app.modules.js
@@ -15,9 +15,6 @@ angular.module('dockermon', [
     'imageDetails'
 ])
     .run(function ($rootScope) {
-        //$rootScope.host = "http://devft-docker-host-02.web.zooplus.de:2375";
-        //$rootScope.host = "http://192.168.100.29:2375";
-        //
         //$rootScope.sidemenu = 'app/shared/sidemenu.html';
         console.log("Rootscope initialized");
     }).config(['$routeProvider', function ($routeProvider) {
