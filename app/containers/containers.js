3aab2eccb8500164bc387bda1aaa1ad636a4da35
Robert Stern <lexandro2000@gmail.com>
2015-03-24 15:50:57 +0100

    Adding empty host name protection to containers

--- app/containers/containers.js
+++ app/containers/containers.js
@@ -7,36 +7,39 @@ angular.module('containers', ['ngRoute'])
             controller: 'ContainerCtrl'
         });
     }])
-    .controller('ContainerCtrl', ['$rootScope', '$scope', '$location', 'Containers', function ($rootScope, $scope, $location, Containers) {
+    .controller('ContainerCtrl', ['$rootScope', '$scope', '$location', 'Helpers', 'Docker', function ($rootScope, $scope, $location, Helpers, Docker) {
+        if (Helpers.isEmpty($rootScope.host)) {
+            $location.path('/hosts');
+        } else {
 
-        var containers = Containers.query(function () {
-            var containerDataList = [];
-            containers.forEach(function (container) {
-                var containerData = {};
-                containerData.Id = container.Id;
-                containerData.Image = container.Image;
-                containerData.Command = container.Command;
-                containerData.Created = container.Created;
-                containerData.Status = container.Status;
-                containerData.Ports = container.Ports;
-                containerDataList.push(containerData);
+            var containers = Docker.containers().query(function () {
+                var containerDataList = [];
+                containers.forEach(function (container) {
+                    var containerData = {};
+                    containerData.Id = container.Id;
+                    containerData.Image = container.Image;
+                    containerData.Command = container.Command;
+                    containerData.Created = container.Created;
+                    containerData.Status = container.Status;
+                    containerData.Ports = container.Ports;
+                    containerDataList.push(containerData);
 
-                var containerDetails = Containers.get({containerId: container.Id}, function () {
-                    $scope.containerDetails = containerDetails;
-                    containerData.ImageId = containerDetails.Image;
-                    containerData.Name = containerDetails.Name.substr(1);
+                    var containerDetails = Docker.containers().get({containerId: container.Id}, function () {
+                        $scope.containerDetails = containerDetails;
+                        containerData.ImageId = containerDetails.Image;
+                        containerData.Name = containerDetails.Name.substr(1);
+                    });
+
                 });
-
+                $scope.containerDataList = containerDataList;
             });
-            $scope.containerDataList = containerDataList;
-        });
-        //
-        $scope.goContainerDetails = function (path) {
-            $location.path('/containerDetails/' + path);
-        };
-        //
-        $scope.goImageDetails = function (path) {
-            $location.path('/imageDetails/' + path);
-        };
-    }])
-;
+            //
+            $scope.goContainerDetails = function (path) {
+                $location.path('/containerDetails/' + path);
+            };
+            //
+            $scope.goImageDetails = function (path) {
+                $location.path('/imageDetails/' + path);
+            };
+        }
+    }]);
