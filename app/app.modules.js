b2c1d4ba8c2e8860a4d67ea06faac1b05ae3784a
Robert Stern <lexandro2000@gmail.com>
2015-03-24 15:52:54 +0100

    Make new services available

--- app/app.modules.js
+++ app/app.modules.js
@@ -8,18 +8,21 @@ angular.module('dockermon', [
     'ngResource',
     'services',
     'hosts',
+    'hostDetails',
     'containers',
     'containerDetails',
     'images',
     'imageDetails'
 ])
     .run(function ($rootScope) {
-        $rootScope.host = "http://devft-docker-host-02.web.zooplus.de:2375";
-        $rootScope.host = "http://192.168.100.29:2375";
+        //$rootScope.host = "http://devft-docker-host-02.web.zooplus.de:2375";
+        //$rootScope.host = "http://192.168.100.29:2375";
         //
         //$rootScope.sidemenu = 'app/shared/sidemenu.html';
         console.log("Rootscope initialized");
-    })
+    }).config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/hosts'});
+    }])
 
 ;
 
