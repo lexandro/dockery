dfdce9464fd019f71021286cb673844102a0869b
Robert Stern <lexandro2000@gmail.com>
2015-03-24 15:51:36 +0100

    Refactoring services. Docker gives all host related stuff and other helper/persistence services defined

--- app/shared/services.js
+++ app/shared/services.js
@@ -1,27 +1,88 @@
 angular.module('services', [])
-    .factory('Containers', ['$resource', '$rootScope',
-        function ($resource, $rootScope) {
-            return $resource($rootScope.host + '/containers/:containerId/json', {}, {
-                query: {
-                    method: 'GET',
-                    isArray: true
-                },
-                get: {
-                    method: 'GET',
-                    isArray: false
+    .factory('Helpers', function () {
+        return {
+            isEmpty: function (obj) {
+                var hasOwnProperty = Object.prototype.hasOwnProperty;
+                // null and undefined are "empty"
+                if (obj == null) return true;
+                // Assume if it has a length property with a non-zero value
+                // that that property is correct.
+                if (obj.length > 0)    return false;
+                if (obj.length === 0)  return true;
+                // Otherwise, does it have any properties of its own?
+                // Note that this doesn't handle
+                // toString and valueOf enumeration bugs in IE < 9
+                for (var key in obj) {
+                    if (hasOwnProperty.call(obj, key)) return false;
                 }
-            });
-        }])
-
-    .factory('Images', function ($resource, $rootScope) {
-        return $resource($rootScope.host + '/images/:imageId/json', {
-            query: {
-                method: "GET",
-                isArray: true
+                return true;
             },
-            get: {
-                method: "GET",
-                isArray: false
+            hasTrueFlag: function hasTrueFlag(object, propertyName) {
+                return !!(object.hasOwnProperty(propertyName) && object[propertyName] == true);
             }
-        });
-    });
+        }
+    })
+    .factory('Docker', function ($resource, $rootScope) {
+        return {
+            containers: function () {
+                return $resource($rootScope.host + '/containers/:containerId/json', {
+                    query: {
+                        method: "GET",
+                        isArray: true
+                    },
+                    get: {
+                        method: "GET",
+                        isArray: false
+                    }
+                });
+            }, images: function () {
+                //console.log('root ' + JSON.stringify($rootScope));
+                return $resource($rootScope.host + '/images/:imageId/json', {
+                    query: {
+                        method: "GET",
+                        isArray: true
+                    },
+                    get: {
+                        method: "GET",
+                        isArray: false
+                    }
+                });
+            },
+            info: function () {
+                //console.log('root ' + JSON.stringify($rootScope));
+                return $resource($rootScope.host + '/info', {
+                    get: {
+                        method: "GET",
+                        isArray: false
+                    }
+                });
+            },
+            version: function () {
+                //console.log('root ' + JSON.stringify($rootScope));
+                return $resource($rootScope.host + '/version', {
+                    get: {
+                        method: "GET",
+                        isArray: false
+                    }
+                });
+            }
+        }
+    })
+    .factory('HostService', ['$rootScope', function ($rootScope) {
+        return {
+            load: function () {
+                var hosts = JSON.parse(localStorage.getItem("hosts"));
+                hosts.forEach(function (host) {
+                    if (host.default) {
+                        $rootScope.host = host;
+                    }
+                });
+                return hosts;
+            },
+            save: function (hosts) {
+                localStorage.setItem("hosts", JSON.stringify(hosts));
+            }
+        }
+    }]);
+
+
