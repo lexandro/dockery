4dbfe8f11bd9984b51ae0fbb666e6a6c49a0fccb
Robert Stern <lexandro2000@gmail.com>
2015-03-27 16:57:04 +0100

    Adding text shortener filter

--- app/app.filters.js
+++ app/app.filters.js
@@ -9,6 +9,39 @@ angular.module('filters', [])
                 number = Math.floor(Math.log(bytes) / Math.log(1024));
             return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
         }
+    })
+    .filter('shorten', function () {
+        /*
+         From:
+         http://stackoverflow.com/questions/18095727/limit-the-length-of-a-string-with-angularjs
+         Usage:
+
+         {{some_text | cut:true:100:' ...'}}
+
+         Options:
+
+         wordwise (boolean) - if true, cut only by words bounds,
+         max (integer) - max length of the text, cut to this number of chars,
+         tail (string, default: ' …') - add this string to the input string if the string was cut.
+         */
+
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || ' ...');
+        };
     });
 
 
