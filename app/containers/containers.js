7d6e8dc64436326209e9f01841afc81f1bf47c41
lexandro <lexandro2000@gmail.com>
2015-04-02 00:27:03 +0200

    Implementing show all/show size flags for container listing

--- app/containers/containers.js
+++ app/containers/containers.js
@@ -11,12 +11,24 @@ angular.module('containers', ['ngRoute'])
         if (Helpers.isEmpty($rootScope.hostUrl)) {
             $location.path('/hosts');
         } else {
+            $scope.showAllContainersFlag = false;
             $scope.showContainerSizeFlag = false;
             refreshContainers();
 
             $scope.refreshContainers = function () {
                 refreshContainers();
             };
+            //
+            $scope.switchShowAllContainersFlag = function () {
+                $scope.showAllContainersFlag = !$scope.showAllContainersFlag;
+                refreshContainers();
+            };
+            $scope.switchShowContainerSizeFlag = function () {
+                console.log('sizeswitch');
+                $scope.showContainerSizeFlag = !$scope.showContainerSizeFlag;
+                refreshContainers();
+            };
+
             //
             $scope.goContainerDetails = function (path) {
                 $location.path('/containerDetails/' + path);
@@ -38,13 +50,18 @@ angular.module('containers', ['ngRoute'])
 
         function refreshContainers() {
             var containerDataList = {};
-            var containers = Docker.containers().query(function () {
+            var containerParam = {};
+            if ($scope.showAllContainersFlag == true) {
+                containerParam.all = 1;
+            }
+            var containers = Docker.containers().query(containerParam, function () {
                 containers.forEach(function (container) {
                     var containerData = {};
 
                     containerData.Id = container.Id;
                     containerData.Image = container.Image;
                     containerData.Created = container.Created;
+                    containerData.Command = container.Command;
                     containerData.Status = container.Status;
                     containerData.SizeRw = 0;
                     containerData.SizeRootFs = 0;
@@ -60,16 +77,27 @@ angular.module('containers', ['ngRoute'])
                 $scope.containerDataList = containerDataList;
                 $scope.containerDataSize = getObjectPropertiesAmount(containerDataList);
             });
-            var containersWithSize = Docker.containers().query({sizeFlag: 1}, function () {
-                containersWithSize.forEach(function (containerWithSize) {
-                    var containerData = containerDataList[containerWithSize.Id];
-                    if (containerWithSize.Id == containerData.Id) {
-                        containerData.SizeRw = containerWithSize.SizeRw;
-                        containerData.SizeRootFs = containerWithSize.SizeRootFs;
-                    }
+            if ($scope.showContainerSizeFlag == true) {
+                console.log('Size ' + $scope.showContainerSizeFlag);
+                containerParam = {};
+                if ($scope.showAllContainersFlag == true) {
+                    containerParam.all = 1;
+                }
+                if ($scope.showContainerSizeFlag == true) {
+                    containerParam.size = 1;
+                }
+                console.log(JSON.stringify(containerParam));
+                var containersWithSize = Docker.containers().query(containerParam, function () {
+                    containersWithSize.forEach(function (containerWithSize) {
+                        var containerData = containerDataList[containerWithSize.Id];
+                        if (containerWithSize.Id == containerData.Id) {
+                            containerData.SizeRw = containerWithSize.SizeRw;
+                            containerData.SizeRootFs = containerWithSize.SizeRootFs;
+                        }
+                    });
+                    $scope.containerDataList = containerDataList;
+                    $scope.containerDataSize = getObjectPropertiesAmount(containerDataList);
                 });
-                $scope.containerDataList = containerDataList;
-                $scope.containerDataSize = getObjectPropertiesAmount(containerDataList);
-            });
+            }
         }
     }]);
