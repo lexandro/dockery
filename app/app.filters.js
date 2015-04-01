e4cbc35ed811844916bb6566dfcc2ca39ed6b376
lexandro <lexandro2000@gmail.com>
2015-04-01 06:55:23 +0200

    Adding precision to size fields and merge them into one

--- app/app.filters.js
+++ app/app.filters.js
@@ -4,7 +4,7 @@ angular.module('filters', [])
     .filter('bytes', function () {
         return function (bytes, precision) {
             if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-            if (bytes == 0 || isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+            if (bytes == 0 ) return '0';
             var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
                 number = Math.floor(Math.log(bytes) / Math.log(1024));
             return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
