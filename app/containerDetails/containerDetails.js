a6d583f28eb29920759dfd3c33be585f19897fb0
Robert Stern <lexandro2000@gmail.com>
2015-04-07 18:15:29 +0200

    Showing diff of a container (first 31 items yet...)

--- app/containerDetails/containerDetails.js
+++ app/containerDetails/containerDetails.js
@@ -13,9 +13,47 @@ angular.module('containerDetails', ['ngRoute'])
         if (Helpers.isEmpty($rootScope.hostUrl)) {
             $location.path('/hosts');
         } else {
+            $scope.activeTab = 'logs';
             var containerDetails = Docker.containers().get({containerId: $routeParams.containerId}, function () {
                 $scope.containerDetails = containerDetails;
             });
+
+            $scope.showLogs = function (containerId) {
+                $scope.activeTab = 'logs';
+                console.log(containerId);
+            };
+
+            $scope.showProcesses = function (containerId) {
+                $scope.activeTab = 'ps';
+                console.log(containerId);
+            };
+
+
+            $scope.showDiff = function (containerId) {
+                $scope.activeTab = 'diff';
+                console.log("loading diffs");
+
+                var containerDiffs = Docker.containers().diff({containerId: containerId}, function () {
+                    var subDiffs = [];
+                    for (var i = 0; i < 30; i++) {
+                        subDiffs.push(containerDiffs[i]);
+                    }
+                    $scope.containerDiffs = subDiffs;
+                    console.log("loaded diffs");
+                });
+
+            };
+
+            $scope.showTty = function (containerId) {
+                $scope.activeTab = 'tty';
+                console.log(containerId);
+            };
+
+
+            $scope.showJson = function (containerId) {
+                $scope.activeTab = 'json';
+                console.log(containerId);
+            };
         }
     }])
 ;
