a9feceeef9df65d79a2acce47141c92b01b1d8aa
Robert Stern <lexandro2000@gmail.com>
2015-04-08 16:05:13 +0200

    Implemented basic paging for container diff

--- app/containerDetails/containerDetails.js
+++ app/containerDetails/containerDetails.js
@@ -14,10 +14,21 @@ angular.module('containerDetails', ['ngRoute'])
             $location.path('/hosts');
         } else {
             $scope.activeTab = 'logs';
+            $scope.newDiffPageSize = 20;
+            //
+            var diffSettings = [];
+            diffSettings["pageIndex"] = 1;
+            diffSettings["maxPageIndex"] = 0;
+            diffSettings["startIndex"] = 0;
+            diffSettings["endIndex"] = 0;
+            diffSettings["pageSize"] = $scope.newDiffPageSize;
+            $scope.diffSettings = diffSettings;
+            //
             var containerDetails = Docker.containers().get({containerId: $routeParams.containerId}, function () {
                 $scope.containerDetails = containerDetails;
             });
-
+            //
+            //
             $scope.showLogs = function (containerId) {
                 $scope.activeTab = 'logs';
                 console.log(containerId);
@@ -28,22 +39,50 @@ angular.module('containerDetails', ['ngRoute'])
                 console.log(containerId);
             };
 
-
             $scope.showDiff = function (containerId) {
                 $scope.activeTab = 'diff';
-                $scope.currentDiffPage = 0;
-                console.log("loading diffs");
-
                 var containerDiffs = Docker.containers().diff({containerId: containerId}, function () {
+                    updateDiffPagedList();
+                });
+                //
+                var updateDiffPagedList = function () {
+                    var diffPageSize = $scope.newDiffPageSize;
+                    diffSettings["pageSize"] = diffPageSize;
+                    diffSettings["maxPageIndex"] = Math.trunc(containerDiffs.length / diffPageSize) + 1;
+                    diffSettings["pageIndex"] = Math.min(diffSettings["maxPageIndex"], diffSettings["pageIndex"]);
+                    diffSettings["count"] = containerDiffs.length;
                     var subDiffs = [];
-                    for (var i = 0; i < 30 && i < containerDiffs.length; i++) {
+                    var startIndex = containerDiffs.length > 0 ? (diffSettings["pageIndex"] - 1) * diffPageSize + 1 : 0;
+                    var endIndex = Math.min((diffSettings["pageIndex"]) * diffPageSize, containerDiffs.length);
+
+                    diffSettings["startIndex"] = startIndex;
+                    diffSettings["endIndex"] = endIndex;
+
+                    for (var i = startIndex - 1; i < endIndex; i++) {
                         subDiffs.push(containerDiffs[i]);
                     }
                     $scope.containerDiffs = subDiffs;
-                    console.log("loaded diffs");
-                });
+                };
 
+                $scope.changeDiffPaging = function () {
+                    if ($scope.newDiffPageSize != diffSettings["pageSize"]) {
+                        updateDiffPagedList();
+                    }
+                };
+                $scope.prevDiffPage = function () {
+                    if (diffSettings["pageIndex"] > 1) {
+                        diffSettings["pageIndex"]--;
+                        updateDiffPagedList();
+                    }
+                };
+                $scope.nextDiffPage = function () {
+                    if (diffSettings["pageIndex"] < diffSettings["maxPageIndex"]) {
+                        diffSettings["pageIndex"]++;
+                        updateDiffPagedList();
+                    }
+                };
             };
+
 
             $scope.showTty = function (containerId) {
                 $scope.activeTab = 'tty';
