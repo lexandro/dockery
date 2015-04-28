f754e15f208eb8d40d6ad6e98e3dedbca23861ca
lexandro <lexandro2000@gmail.com>
2015-04-28 13:08:02 +0200

    Starting implementation create container

---
+++ app/createContainer/createContainer.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('containerDetails', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/createContainer', {
+            templateUrl: 'app/createContainer/createContainer.html',
+            controller: 'CreateContainerCtrl'
+        });
+    }])
+
+    .controller('CreateContainerCtrl', ['$rootScope', '$scope', '$location', '$routeParams', 'Helpers', 'Docker', function ($rootScope, $scope, $location, $routeParams, Helpers, Docker) {
+        if (Helpers.isEmpty($rootScope.hostUrl)) {
+            $location.path('/hosts');
+        } else {
+        }
+    }]);
