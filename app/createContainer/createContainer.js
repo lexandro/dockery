c43a3385d2cf8710c4f7017d89841a025ef4a981
lexandro <lexandro2000@gmail.com>
2015-04-29 01:13:41 +0200

    Fixing module name

--- app/createContainer/createContainer.js
+++ app/createContainer/createContainer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('containerDetails', ['ngRoute'])
+angular.module('createContainer', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/createContainer', {
