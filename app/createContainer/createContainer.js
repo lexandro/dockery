f372a4e1a3e73c502394ba86e704fa670d61d67f
lexandro <lexandro2000@gmail.com>
2015-05-07 00:46:31 +0200

    Basic settings to create containers added

--- app/createContainer/createContainer.js
+++ app/createContainer/createContainer.js
@@ -11,5 +11,23 @@ angular.module('createContainer', ['ngRoute'])
         if (Helpers.isEmpty($rootScope.hostUrl)) {
             $location.path('/hosts');
         } else {
+            $scope.imageName = 'ubuntu';
+            $scope.rm = false;
+            $scope.detach = true;
+            $scope.tty = true;
+            $scope.interactive = true;
+            $scope.privileged = false;
+            //
+            $scope.createContainer = function () {
+                console.log('create');
+            };
+            $scope.createAndStartContainer = function () {
+                $scope.createContainer();
+                console.log('and start');
+            };
+            //
+            $scope.generateContainerName = function () {
+                $scope.newContainerName = 'random_container';
+            };
         }
     }]);
