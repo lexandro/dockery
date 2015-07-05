b8cc13d5ffe9079e49fbc9cfe590a3d36c407016
lexandro <lexandro2000@gmail.com>
2015-07-06 01:03:35 +0200

    TOTALLY boilerplate

--- app/repository/repository.js
+++ app/repository/repository.js
@@ -14,12 +14,41 @@ angular.module('repository', ['ngRoute'])
             $location.path('/hosts');
         } else {
             $scope.searchImageName = '';
+            $scope.repositorySearching = false;
+            $scope.repositorySearchingError = false;
             $scope.searchImage = function () {
-                console.log('Search with: ' + $scope.searchImageName);
-                var searchResults = Docker.images().search({term: $scope.searchImageName}, function () {
-                    console.log(JSON.stringify(searchResults));
+                $scope.repositorySearching = true;
+                $scope.repositorySearchingError = false;
+                $scope.searchText = $scope.searchImageName;
+                var searchResults = Docker.images().search({term: $scope.searchText}, function () {
+                    $scope.repositorySearching = false;
                     $scope.searchResults = searchResults;
+                }, function (error) {
+                    $scope.repositorySearching = false;
+                    $scope.repositorySearchingError = true;
+                    $scope.error = error;
                 });
+
+
+            }
+            $scope.downloadImage = function (imageName) {
+                oboe({
+                    url: 'http://localhost:2375/images/create?fromImage=lexandro/echo-repeat&tag=latest',
+                    method: 'POST'
+                })
+                    .node('*', function (item) {
+                        if (typeof item == "object" && !Helpers.isEmpty(item) && !Helpers.isEmpty(item.status)) {
+                            console.log("Received: " + JSON.stringify(item));
+                            if (item.status === "Status: Downloaded newer image for lexandro/echo-repeat:latest") {
+                                console.log('Finito');
+                                this.abort();
+                            }
+                        }
+                    })
+                    .done(function (things) {
+                        console.log('there are', things.item.length, 'things to read');
+                    });
+                console.log('download ' + imageName);
             }
         }
     }]);
