ab5524836eb7e6a3ce9d0c60482c024b4c51e69a
lexandro <lexandro2000@gmail.com>
2015-07-07 01:02:02 +0200

    Working on image pulling

--- app/repository/repository.js
+++ app/repository/repository.js
@@ -16,6 +16,7 @@ angular.module('repository', ['ngRoute'])
             $scope.searchImageName = '';
             $scope.repositorySearching = false;
             $scope.repositorySearchingError = false;
+            $scope.selectAllImagesFlag = false;
             $scope.searchImage = function () {
                 $scope.repositorySearching = true;
                 $scope.repositorySearchingError = false;
@@ -23,6 +24,10 @@ angular.module('repository', ['ngRoute'])
                 var searchResults = Docker.images().search({term: $scope.searchText}, function () {
                     $scope.repositorySearching = false;
                     $scope.searchResults = searchResults;
+                    searchResults.forEach(function (searchResult) {
+                        searchResults.selected = false;
+                    });
+
                 }, function (error) {
                     $scope.repositorySearching = false;
                     $scope.repositorySearchingError = true;
@@ -30,25 +35,52 @@ angular.module('repository', ['ngRoute'])
                 });
 
 
-            }
+            };
             $scope.downloadImage = function (imageName) {
-                oboe({
-                    url: 'http://localhost:2375/images/create?fromImage=lexandro/echo-repeat&tag=latest',
-                    method: 'POST'
-                })
-                    .node('*', function (item) {
-                        if (typeof item == "object" && !Helpers.isEmpty(item) && !Helpers.isEmpty(item.status)) {
-                            console.log("Received: " + JSON.stringify(item));
-                            if (item.status === "Status: Downloaded newer image for lexandro/echo-repeat:latest") {
-                                console.log('Finito');
-                                this.abort();
-                            }
-                        }
+                toastr["info"]('Pulling image ' + imageName, 'Check the progress on tasks');
+                if (imageName == 'lexandro/echo-repeat') {
+                    oboe({
+                        url: 'http://localhost:2375/images/create?fromImage=lexandro/echo-repeat&tag=latest',
+                        method: 'POST'
                     })
-                    .done(function (things) {
-                        console.log('there are', things.item.length, 'things to read');
-                    });
+                        .node('*', function (item) {
+                            if (typeof item == "object" && !Helpers.isEmpty(item) && !Helpers.isEmpty(item.status)) {
+                                console.log("Received: " + JSON.stringify(item));
+                                if (item.status === "Status: Downloaded newer image for lexandro/echo-repeat:latest") {
+                                    console.log('Finito');
+                                    this.abort();
+                                }
+                            }
+                        })
+                        .done(function (things) {
+                            console.log('there are', things.item.length, 'things to read');
+                        });
+                }
                 console.log('download ' + imageName);
+            };
+
+            $scope.downloadSelectedImages = function () {
+                var searchResults = $scope.searchResults;
+                searchResults.forEach(function (searchResult) {
+                    if (searchResult.selected) {
+                        console.log(JSON.stringify(searchResult));
+                        $scope.downloadImage(searchResult.name);
+                    }
+                });
+            };
+
+            $scope.switchSelectAllImagesFlag = function () {
+                $scope.selectAllImagesFlag = !$scope.selectAllImagesFlag;
+                var searchResults = $scope.searchResults;
+                searchResults.forEach(function (searchResult) {
+                    searchResult.selected = $scope.selectAllImagesFlag;
+                });
+                $scope.searchResults = searchResults;
+            };
+
+            $scope.selectImageFlag = function (image) {
+                image.selected = !image.selected;
+                $scope.selectAllImagesFlag = false;
             }
         }
     }]);
