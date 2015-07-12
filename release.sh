2ce6aeae152077d0c4ba2e10300318dff4a3cc05
lexandro <lexandro2000@gmail.com>
2015-07-12 10:57:58 +0200

    Fixing release script

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 echo Preparing Dockery release
 rm dockery.zip
-zip -9r dockery.zip app assets backgound.js manifest.json index.html window.html 
+zip -9r dockery.zip app assets background.js manifest.json index.html window.html
 
